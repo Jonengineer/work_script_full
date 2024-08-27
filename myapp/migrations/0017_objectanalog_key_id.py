66024d03d8cb97c40e4b0cd32a0eb9dab654cf12
jonengineer <jonengineer@mail.ru>
2024-08-27 12:02:16 +0300

    Финальная таблица

--- myapp/migrations/0017_objectanalog_key_id.py
+++ myapp/migrations/0017_objectanalog_key_id.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='objectanalog',
             name='key_id',
-            field=models.ForeignKey(default=5, on_delete=django.db.models.deletion.CASCADE, to='myapp.expensestoepcmap'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='myapp.expensestoepcmap'),
             preserve_default=False,
         ),
     ]
